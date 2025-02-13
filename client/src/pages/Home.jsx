d0abe4fb7cb5e9b32ef046e40f5eb89e68d37ea9
praneeth-katuri <praneethkaturi@outlook.com>
2025-02-13 19:30:47 +0530

    feat: Add Home component to pages

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import HeroSection from "@/components/HeroSection";
+import WhyChooseSection from "@/components/WhyChooseSection";
+import TestimonialsSection from "@/components/TestimonialsSection";
+
+const Home = () => (
+  <>
+    <HeroSection />
+    <WhyChooseSection />
+    <TestimonialsSection />
+  </>
+);
+
+export default Home;
