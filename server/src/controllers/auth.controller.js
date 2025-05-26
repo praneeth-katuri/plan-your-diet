3acff7b06bf8ec80ff24b3d9ba02a40a12e15ec5
Praneeth Katuri <132742639+praneeth-katuri@users.noreply.github.com>
2025-05-26 18:40:51 +0530

    fix(controller): Fix error undefined issue (#7)
    - Fix error not catched in try...catch block
    - Update .gitignore
    
    Co-authored-by: Amrutha Katuri <50200591+amrutha-katuri@users.noreply.github.com>

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -4,16 +4,16 @@ const register = async (req, res) => {
   try {
     const user = await authService.register(req.body);
     res.status(201).json(user);
-  } catch {
+  } catch (error) {
     res.status(400).json({ error: error.message });
   }
 };
 
 const login = async (req, res) => {
   try {
-    const token = await authService.body(res.body);
+    const token = await authService.login(req.body);
     res.status(200).json({ token });
-  } catch {
+  } catch (error) {
     res.status(401).json({ error: error.message });
   }
 };
