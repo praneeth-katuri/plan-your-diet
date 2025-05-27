d3eff48cbcfc46bfcedf345d56a313b9884e18a4
Praneeth Katuri <132742639+praneeth-katuri@users.noreply.github.com>
2025-05-27 12:56:27 +0530

    chore: Use Links instead of anchor tags (#13)
    - Used Links form react-router-dom instead of anchor tags for navigation
    
    Co-authored-by: Amrutha Katuri <50200591+amrutha-katuri@users.noreply.github.com>

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import "./AuthPages.css";
 import RegisterBg from "../assets/images/auth_bg.png";
 
@@ -74,7 +75,7 @@ const RegisterPage = () => {
               Register as {role.charAt(0).toUpperCase() + role.slice(1)}
             </button>
             <p className="small-text">
-              Already have an account? <a href="#">Login here</a>
+              Already have an account? <Link to="/login">Login here</Link>
             </p>
           </form>
         </div>
