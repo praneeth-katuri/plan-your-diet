7c691d80973c683a695a53b7b85e085bdcf97684
Praneeth Katuri <132742639+praneeth-katuri@users.noreply.github.com>
2025-05-29 21:00:46 +0530

    feat: Add Spinner component

---
+++ client/src/components/Spinner.jsx
@@ -0,0 +1,7 @@
+const Spinner = () => (
+  <div className="flex justify-center items-center h-screen">
+    <div className="w-8 h-8 border-4 border-gray-300 border-t-indigo-600 rounded-full animate-spin" />
+  </div>
+);
+
+export default Spinner;
