98dd44b62a36261f7951d632144dcfb5be047003
Praneeth Katuri <132742639+praneeth-katuri@users.noreply.github.com>
2025-05-29 21:03:18 +0530

    chore: refactor app.js
    - added all the routes into the express app

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const authRoutes = require("./routes/auth.routes");
+const userRoutes = require("./routes/user.routes");
+const dietRoutes = require("./routes/diet.routes");
 
 const app = express();
-
 //Middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 
 // Routes here
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/diet", dietRoutes);
 
 module.exports = app;
