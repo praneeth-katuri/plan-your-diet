79842e3d86b652fd72b449f277c697920c9ad285
Praneeth Katuri <132742639+praneeth-katuri@users.noreply.github.com>
2025-05-29 20:52:24 +0530

    chore: Add State and form onSubmit
    - Managed form data using State
    - Added handleChange to update inputs
    - Added HandleSubmit for form submission

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,10 +1,40 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
+import { useAuth } from "@/context/AuthContext";
+import api from "@/api/axios";
 import "./AuthPages.css";
 import RegisterBg from "../assets/images/auth_bg.png";
+import Spinner from "@/components/Spinner";
 
 const RegisterPage = () => {
+  const { isAuthenticated, isLoading } = useAuth();
   const [role, setRole] = useState("member");
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await api.post("/auth/register", formData);
+      console.log("success", res.data);
+    } catch (error) {
+      console.log("Error:", error.response?.message);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  if (isLoading) return <Spinner />;
+
+  if (isAuthenticated) return <Navigate to="/diet" />;
 
   return (
     <section
@@ -32,44 +62,25 @@ const RegisterPage = () => {
             </span>
           </div>
 
-          <form>
+          <form onSubmit={handleSubmit}>
             <input type="text" name="name" placeholder="Full Name" required />
             <input
               type="email"
               name="email"
+              value={formData.email}
               placeholder="Email Address"
+              onChange={handleChange}
               required
             />
             <input
               type="password"
               name="password"
+              value={formData.password}
               placeholder="Password (min 8 chars)"
+              onChange={handleChange}
               minLength="8"
               required
             />
-
-            {role === "member" && (
-              <>
-                <input
-                  type="number"
-                  name="age"
-                  placeholder="Age (in years)"
-                  required
-                />
-                <input
-                  type="number"
-                  name="height"
-                  placeholder="Height (cm)"
-                  required
-                />
-                <input
-                  type="number"
-                  name="weight"
-                  placeholder="Weight (kg)"
-                  required
-                />
-              </>
-            )}
 
             <button type="submit" className="btn btn-primary">
               Register as {role.charAt(0).toUpperCase() + role.slice(1)}
