5781ab8b01908d55deffde7b7866a765403adb3c
Praneeth Katuri <132742639+praneeth-katuri@users.noreply.github.com>
2025-05-29 21:01:16 +0530

    feat: Update Routes and ProtectedRoute
    - added new routes to AppRoutes
    - added spinner and login state to ProtectedRoute

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -5,6 +5,9 @@ import FoodFacts from "@/pages/FoodFacts";
 import LoginPage from "@/pages/LoginPage";
 import ForgotPasswordPage from "@/pages/ForgotPasswordPage";
 import RegisterPage from "@/pages/RegisterPage";
+import ProfileSetup from "@/pages/ProfileSetup";
+import ProtectedRoute from "./ProtectedRoute";
+import DietPlan from "@/pages/DietPlan";
 
 const AppRoutes = () => (
   <Routes>
@@ -12,7 +15,22 @@ const AppRoutes = () => (
       <Route index element={<Home />} />
       <Route path="food-facts" element={<FoodFacts />} />
     </Route>
-
+    <Route
+      path="/profile/setup"
+      element={
+        <ProtectedRoute>
+          <ProfileSetup />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/diet"
+      element={
+        <ProtectedRoute>
+          <DietPlan />
+        </ProtectedRoute>
+      }
+    />
     <Route path="/login" element={<LoginPage />} />
     <Route path="/forgot-password" element={<ForgotPasswordPage />} />
     <Route path="/register" element={<RegisterPage />} />
--- client/src/routes/ProtectedRoute.jsx
+++ client/src/routes/ProtectedRoute.jsx
@@ -1,9 +1,15 @@
 import { useAuth } from "../context/AuthContext";
+import Spinner from "@/components/Spinner";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const { accessToken } = useAuth();
-  return accessToken ? children : <Navigate to="/login" />;
+  const { isAuthenticated, isLoading } = useAuth();
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  return isAuthenticated ? children : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
