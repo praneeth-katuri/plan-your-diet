990638cf209298b1ccbd449a476c5fd7f5b9e0b9
Praneeth Katuri <132742639+praneeth-katuri@users.noreply.github.com>
2025-05-29 17:59:59 +0530

    refactor: Simplify Bearer Token Extraction

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -3,14 +3,15 @@ const config = require("../config");
 const prisma = require("../db/client");
 
 const verify = async (req, res, next) => {
-  const token = req.header("Authorization")?.replace("Bearer ", "");
+  const authHeader = req.headers.authorization;
+  const accessToken = authHeader.split(" ")[1];
 
-  if (!token) {
+  if (!accessToken) {
     return res.status(401).json({ message: "No token, authorization denied." });
   }
 
   try {
-    const decoded = jwt.verify(token, config.jwt.secret);
+    const decoded = jwt.verify(accessToken, config.jwt.accessSecret);
     const user = await prisma.user.findUnique({ where: { id: decoded.id } });
 
     if (!user) {
