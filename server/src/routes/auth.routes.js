f7d0ad26562ac43a252295b5c70c48f4fc13e597
Praneeth Katuri <132742639+praneeth-katuri@users.noreply.github.com>
2025-05-31 02:39:18 +0530

    feat: Add endpoint to logout
    - Added an api endpoint to logout user by removing httpOnly cookie

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth.controller");
-const authMiddleware = require("../middlewares/auth.middleware");
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.post("/refresh-token", authController.getAccessToken);
-router.get("/me", authMiddleware, authController.getMe);
+router.post("/logout", authController.logout);
 
 module.exports = router;
