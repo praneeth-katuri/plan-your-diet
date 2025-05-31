376b41b6a590036102a43dec8e664ae7a77418ba
Praneeth Katuri <132742639+praneeth-katuri@users.noreply.github.com>
2025-05-31 17:19:05 +0530

    Refactor/components (#27)
    * refactor: update the timeout for axios instance
    
    * feat: Add DietPlan Component
    
    - moved DietPlan component
    
    * feat: Add ProfileEdit component
    
    - Added component to edit user profile
    
    * fix: Fixed handleSubmit logic
    
    * refactor(server): rename middleware import
    
    * chore: Setup toaster

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
 import AppRoutes from "./routes/AppRoutes";
 import ScrollToTop from "./components/ScrollToTop";
+import { Toaster } from "react-hot-toast";
 import "./App.css";
 
 function App() {
   return (
     <>
+      <Toaster
+        toastOptions={{
+          className: "text-sm font-medium",
+          style: {
+            borderRadius: "8px",
+            background: "#fff",
+            color: "#333",
+          },
+        }}
+        position="top-center"
+        reverseOrder={false}
+      />
       <ScrollToTop />
       <AppRoutes />
     </>
